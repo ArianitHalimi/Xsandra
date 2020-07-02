4a56bae8c58c6c3d4940f6921dbf6f5257640bab
ArianitHalimi <arianit.halimi3@gmail.com>
2020-07-02 19:19:26 +0200

    Window creation with electron

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+const {app, BrowserWindow,Menu} = require('electron')
+
 class Xsandra{
+  width = 1000
+  height = 600
+  createWindow(){
+    const mainWindow = new BrowserWindow({
+      width: this.width,
+      height: this.height,
+      webPreferences: {
+        nodeIntegration: true,
+      }
+    })
+    //mainWindow.loadFile(process.cwd() + '/Game.html')
+  }
+  setWindowSize(width,height){
+    if(arguments.length == 1 && arguments[0]=='fullscreen'){
+      //do full screen resolution
+    }else{
+      this.width = width
+      this.height = height
+    }
+  }
   run(){
-    while(true);
+    app.whenReady().then(() => {
+      this.createWindow()
+      Menu.setApplicationMenu(null)
+
+      app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      })
+    })
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') app.quit()
+    })
   }
 }
 
